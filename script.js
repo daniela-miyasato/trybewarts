9e37a5cd9b696e33b3face4b133f14863fac43ed
Geovani Moura <94002157+GeovaniMoura@users.noreply.github.com>
2022-01-11 15:22:17 -0300

    Requisito 3 - Done

---
+++ script.js
@@ -0,0 +1,14 @@
+const buttonLogin = document.querySelector('#buttonLogin');
+
+function hello() {
+  const email = document.querySelector('#email');
+  const senha = document.querySelector('#senha');
+
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+buttonLogin.addEventListener('click', hello);
