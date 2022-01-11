797d661fd99ecb81869ecd2566d8ec41aaab9605
Geovani Moura <94002157+GeovaniMoura@users.noreply.github.com>
2022-01-11 18:52:33 -0300

    Requisito 20 - Done

--- script.js
+++ script.js
@@ -26,3 +26,36 @@ function ativarButton() {
 }
 
 inputTermos.addEventListener('click', ativarButton);
+
+const textarea = document.querySelector('#textarea');
+
+// let limite = 500;
+
+// function contador(event) {
+//   const counter = document.querySelector('#counter');
+//   if (limite <= 500 && limite >= 0) {
+//     if (event.key === 'Backspace') {
+//       limite += 1;
+
+//       // counter.innerText = limite.toString();
+//     } else {
+//       limite -= 1;
+
+//       // counter.innerText = limite.toString();
+//     }
+//     counter.innerText = limite.toString();
+//   }
+// }
+
+// textarea.addEventListener('keydown', contador);
+
+const limite = 500;
+
+function contador() {
+  if (textarea.value.length <= 500 && textarea.value.length > -1) {
+    const counter = document.querySelector('#counter');
+    counter.innerText = limite - textarea.value.length;
+  }
+}
+
+textarea.addEventListener('keyup', contador);
